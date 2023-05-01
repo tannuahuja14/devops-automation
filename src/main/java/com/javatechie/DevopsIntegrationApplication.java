fd90249d767df43991a95ab0da10f4c5170ca056
Tannu Ahuja <tannu.ahuja@knoldus.com>
2023-05-01 11:17:27 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to javatechie";
+		return "Heyy!!My name is Tannu Ahuja";
 	}
 
 	public static void main(String[] args) {
